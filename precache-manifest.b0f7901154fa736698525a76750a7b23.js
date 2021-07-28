ccb7eaf60c2edd14ab30b0b0af76f9684e393e27
robadillo <robadillo@users.noreply.github.com>
2021-07-28 22:23:42 +0000

    deploy: 7035ea205e4d29f9b1ac90593952c0299119fc5a

---
+++ precache-manifest.b0f7901154fa736698525a76750a7b23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbc5ee0efb98662d241ade523d6f6b3",
+    "url": "https://robadillo.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "dbfc19f5d7e21ee493f5",
+    "url": "https://robadillo.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "0443a9c1336a494605ce",
+    "url": "https://robadillo.github.io/task-management-frontend/static/js/2.bdf9e213.chunk.js"
+  },
+  {
+    "revision": "dbfc19f5d7e21ee493f5",
+    "url": "https://robadillo.github.io/task-management-frontend/static/js/main.aa51ecb1.chunk.js"
+  },
+  {
+    "revision": "7df09b398e90ee141a4b",
+    "url": "https://robadillo.github.io/task-management-frontend/static/js/runtime~main.94bfa2e2.js"
+  }
+]);
